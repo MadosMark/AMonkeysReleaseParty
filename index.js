ba5d0fb0d10c2630fb88ab5b19b7ad07ea1379ef
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-03-03 19:22:44 +0100

    Created second, third and forth page

---
+++ index.js
@@ -0,0 +1,24 @@
+// Rotate function
+
+let vinylDisc = document.querySelector(".vinyl_disc")
+
+setTimeout(animationChanges, 6200); 
+
+function animationChanges() { 
+    vinylDisc.style.transition = `6000ms`
+    vinylDisc.style.animationTimingFunction = "linear";
+    vinylDisc.style.animationDelay = `2s`
+
+}
+
+
+
+
+
+
+
+
+
+// const params = new URLSearchParams(window.location.search);
+
+// https://reactgo.com/javascript-get-query-parameters/?Namn=Mark
