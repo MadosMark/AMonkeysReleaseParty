d146a190fd724b2b25f52adf3b4e5a416935b4c8
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-03-17 11:20:23 +0100

    Form adjustments

--- index.js
+++ index.js
@@ -2,22 +2,21 @@
 
 let vinylDisc = document.querySelector(".vinyl_disc")
 
-setTimeout(animationChanges, 6200); 
+setTimeout(animationChanges, 6200);
 
-function animationChanges() { 
+function animationChanges() {
     vinylDisc.style.transition = `6000ms`
     vinylDisc.style.animationTimingFunction = "linear";
     vinylDisc.style.animationDelay = `2s`
 
 }
 
-
-
-
-
 // Query String
-
 
 const params = new URLSearchParams(window.location.search);
 const name = params.get("name");
 console.log(name);
+
+
+// 
+
