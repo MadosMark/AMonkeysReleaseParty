753e818765ff3fad777ea7cab5541ea92a7b9673
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-03-18 10:54:21 +0100

    Form adjustments

--- form.js
+++ form.js
@@ -9,14 +9,14 @@ const mail = linkup.get("email")
 
 
 let holdah = document.getElementById('mail_holder');
-if (mail) {
-    holdah.value = mail;
-} else {
+if (mail === 'null') {
     holdah.value = ' exempel@exempel.se';
+} else {
+    holdah.value = mail;
 }
 let holla = document.getElementById('name_holder');
-if (namn) {
-    holla.textContent = namn;
-} else {
+if (namn === 'null') {
     holla.textContent = 'Förhoppningsvis dig!'
+} else {
+    holla.textContent = namn;
 }
