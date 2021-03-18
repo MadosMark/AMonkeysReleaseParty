6483e522b06837740613b19a18047d7da0810d85
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-03-18 11:34:48 +0100

    Added animations

--- index.js
+++ index.js
@@ -20,12 +20,15 @@ const email = params.get("email")
 
 let node = document.createTextNode(name + '!');
 let space = document.createTextNode('\u00A0');
+let mark = document.createTextNode('!');
 
 
 let ticket = document.getElementById('text_welcome');
 if (name && email) {
     ticket.appendChild(space);
     ticket.appendChild(node);
+} else {
+  ticket.appendChild(mark);
 }
 
 
@@ -44,3 +47,48 @@ const ticketbutton = document.getElementById('tickets_name');
 ticketbutton.addEventListener("click", function() {
     window.location = url;
   });
+
+
+  // Fade in animations
+
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
